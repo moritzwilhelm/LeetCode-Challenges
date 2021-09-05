6631c7d80d5766bef22cb592dd139d0af0bde2d5
Moritz Wilhelm <s8mowilh@stud.uni-saarland.de>
2021-09-05 15:38:30 +0200

    [SC05] prettified rotation algorithm

--- September Challenge 2021/Orderly Queue/solution.py
+++ September Challenge 2021/Orderly Queue/solution.py
@@ -2,11 +2,5 @@ class Solution:
     def orderlyQueue(self, s: str, k: int) -> str:
         if k > 1:
             return ''.join(sorted(s))
-
-        res = s
-        new_res = res
-        for i in range(1, len(s)):
-            new_res = new_res[1:] + new_res[0]
-            if new_res < res:
-                res = new_res
-        return res
+        else:
+            return min(s[i:] + s[:i] for i in range(len(s)))
